d7518dafd8aa062acfc8dc637eb1503a973394d9
Karl O'Brien <karl.obrien@leaseplan.com>
2024-05-15 14:17:17 +0200

    migrated to Tailwind CSS and Daisy UI

--- src/views/NavButton.jsx
+++ src/views/NavButton.jsx
@@ -2,7 +2,7 @@ import m from "mithril";
 
 const NavButton = {
   view: ({ attrs, children }) => (
-    <div  role="button" className="px-1 mx-0 text-lg btn btn-ghost" onclick={() =>  m.route.set(attrs.path)}>
+    <div  role="button" className="px-1 mx-0 text-xl btn btn-ghost" onclick={() =>  m.route.set(attrs.path)}>
       {children}
     </div>
   ),
